3c4a7bb045dfd154ef8470f8376f964359a42073
Ryan <ryan.mohrman@gmail.com>
2022-06-05 03:48:40 -0600

    Finish Ember Data section

---
+++ app/adapters/application.js
@@ -0,0 +1,10 @@
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
+
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  host = "http://localhost:3000"
+  namespace = 'api';
+
+  buildURL(...args) {
+    return `${super.buildURL(...args)}`;
+  }
+}
